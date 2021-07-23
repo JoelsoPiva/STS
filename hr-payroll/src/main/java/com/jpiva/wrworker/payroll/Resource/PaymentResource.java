72b7ed997d8e46152d55d00a5f299da895253e28
joelsopiva <joelsopiva@gmail.com>
2021-07-23 19:09:13 -0300

    Usando Hystrix- Tratamento a tolerancia a falhas

--- hr-payroll/src/main/java/com/jpiva/wrworker/payroll/Resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/jpiva/wrworker/payroll/Resource/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jpiva.wrworker.payroll.entities.Paymanent;
 import com.jpiva.wrworker.payroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value ="/Payments")
@@ -17,11 +18,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value ="/{workerId}/days/{days}")
 	public ResponseEntity <Paymanent> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Paymanent payment  = service.getPayment(workerId, days );
 		return ResponseEntity.ok(payment);
 	}
 	
+	public ResponseEntity <Paymanent> getPaymentAlternative(Long workerId, Integer days){
+		Paymanent payment  = new Paymanent("Joelso",100.0, days );
+		return ResponseEntity.ok(payment);
+		}
 
 }
